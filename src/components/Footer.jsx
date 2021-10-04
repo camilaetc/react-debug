e7d2e40aa2e473d5ff8913c30a4a937a4c0733fe
camilaetc <91088770+camilaetc@users.noreply.github.com>
2021-10-04 16:44:03 -0300

    hice pie de pagina

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./styles/Footer.css";
 
 const PieDePagina = () => {
-  return <footer>pie de pagina</footer>;
+  return (
+    <footer className="pieDePagina">
+      <p className="parrafoFooter">
+        Todos los derechos reservados 2003-2021. República Argentina. INCAA{" "}
+        <br />
+        Lima 319; Código Postal: C1073AAG, CABA; República Argentina.
+      </p>
+      <ul className="lista-enlaces-externos">
+        <li>
+          <a href="/">
+            <img src="http://cine.ar/img/logo-cinear.svg" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src="http://cine.ar/img/logo-arsat.svg" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src="http://cine.ar/img/logo-incaa.svg" alt="" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default PieDePagina;
